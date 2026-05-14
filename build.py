23756ab6262deab0041209037990557e03f0ac05
ShangKeQian <shangkeqian@126.com>
2026-05-14 19:49:15 +0800

    fix: include MediaPipe data files in PyInstaller build

--- build.py
+++ build.py
@@ -3,16 +3,26 @@
 import subprocess
 import sys
 import os
+import mediapipe
 
 
 def main():
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    mp_path = os.path.dirname(mediapipe.__file__)
 
     cmd = [
         sys.executable, "-m", "PyInstaller",
         "--onefile",
         "--windowed",
         "--name", "GetUp",
+        "--add-data", f"blaze_face_short_range.tflite;.",
+        "--add-data", f"{mp_path}/tasks/c/libmediapipe.dll;mediapipe/tasks/c/",
+        "--add-data", f"{mp_path}/modules;mediapipe/modules/",
+        "--add-data", f"{mp_path}/tasks/metadata;mediapipe/tasks/metadata/",
+        "--hidden-import", "mediapipe",
+        "--hidden-import", "mediapipe.tasks",
+        "--hidden-import", "mediapipe.tasks.python",
+        "--hidden-import", "mediapipe.tasks.python.vision",
         "main.py",
     ]
 
