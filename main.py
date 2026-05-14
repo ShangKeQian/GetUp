f5455d675aab0193c7965c2468b1669f75d3fd9b
ShangKeQian <shangkeqian@126.com>
2026-05-14 22:13:06 +0800

    fix: remove camera_fail_count logic, face detection result directly determines presence

--- main.py
+++ main.py
@@ -84,7 +84,6 @@ class GetUpApp:
         tick_count = 0
         last_camera_found_time = 0
         last_camera_check_time = 0
-        camera_fail_count = 0
         while self._running:
             now = time.time()
             idle_time = now - last_input_time
@@ -94,20 +93,13 @@ class GetUpApp:
 
             if idle_time < 5:
                 person_present = True
-                camera_fail_count = 0
             elif camera_idle < 5:
                 person_present = True
             elif now - last_camera_check_time >= 5:
                 last_camera_check_time = now
-                face_found = self._camera.check_once()
-                if face_found:
+                if self._camera.check_once():
                     person_present = True
                     last_camera_found_time = now
-                    camera_fail_count = 0
-                else:
-                    camera_fail_count += 1
-                    if camera_fail_count < 3:
-                        person_present = True
 
             if person_present:
                 self._timer.on_person_detected()
